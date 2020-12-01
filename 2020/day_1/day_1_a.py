83ddd47ae8b7b404539b33f7a86c715827752e83
ryandkuster <ryan.kuster@gmail.com>
2020-12-01 09:39:37 -0500

    cleaned up code, added pretty stars

--- 2020/day_1/day_1_a.py
+++ 2020/day_1/day_1_a.py
@@ -2,13 +2,6 @@ import pandas as pd
 import sys
 
 
-def main():
-    exp_report = pd.read_csv(sys.argv[1], header=None, names=['entry'])
-    exp_report['2020_minus_entry'] = 2020 - exp_report['entry']
-    answer = find_2020_combos(exp_report)
-    print(answer)
-
-
 def find_2020_combos(exp_report):
     for i in exp_report['entry'].values:
         if i in exp_report['2020_minus_entry'].values:
@@ -16,4 +9,6 @@ def find_2020_combos(exp_report):
 
 
 if __name__ == '__main__':
-    main()
+    exp_report = pd.read_csv(sys.argv[1], header=None, names=['entry'])
+    exp_report['2020_minus_entry'] = 2020 - exp_report['entry']
+    print(find_2020_combos(exp_report))
