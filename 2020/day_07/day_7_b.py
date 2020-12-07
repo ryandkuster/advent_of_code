b02662c9c8f7b2b1876d703e482ec8f1e6a823c0
ryandkuster <ryan.kuster@gmail.com>
2020-12-07 15:26:13 -0500

    2020: 7

--- 2020/day_07/day_7_b.py
+++ 2020/day_07/day_7_b.py
@@ -10,33 +10,34 @@ def baggage_claim(f):
         inner, outer = bag_handler(rule, bag_dt)
         bag_dt = bag_dt_init(outer, inner, bag_dt)
     bag_ls = bag_dt_search(bag_dt, ['shiny gold'], bag_ls)
-    #print(len(set(bag_ls)))
-    print(len(set(bag_ls)))
+    print(len(bag_ls))
 
 
 def bag_handler(rule, bag_dt):
     rule = re.sub(r'( bags| bag)', '', rule)
     outer, inner = rule.split(' contain ')
-    inner = re.sub(r'\d ', '', inner)
     inner = inner.split(', ')
     return inner, outer
 
 
 def bag_dt_init(outer, inner, bag_dt):
-    bag_dt[outer] = {bag: None for bag in inner}
+    bag_dt[outer] = [bag for bag in inner]
     return bag_dt
 
 
 def bag_dt_search(bag_dt, target_ls, bag_ls):
     new_target_ls = []
     for target in target_ls:
-        for k1, v1 in bag_dt.items():
-            for k2, v2 in v1.items():
-                if target in k2:
-                    bag_ls.append(k1)
-                    new_target_ls.append(k1)
+        for val in bag_dt[target]:
+            try:
+                num = int(re.findall(r'[0-9]+', val)[0])
+                color = ' '.join(val.split(' ')[1:])
+                this_turn = [color for i in range(num)]
+            except IndexError:
+                continue
+            new_target_ls = new_target_ls + this_turn
+            bag_ls = bag_ls + this_turn
     if len(new_target_ls) > 0:
-        #print(new_target_ls)
         return bag_dt_search(bag_dt, new_target_ls, bag_ls)
     else:
         return bag_ls
